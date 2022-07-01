390c673f786f37c2f4ea192124ed099ac618687b
AlfonsoOrtiz98 <108146246+AlfonsoOrtiz98@users.noreply.github.com>
2022-07-01 08:45:58 -0500

    Add files via upload

---
+++ Analizis 1/Analizis 1/Form1.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.pcbGrafico = New System.Windows.Forms.PictureBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.lblPuntos = New System.Windows.Forms.Label()
+        Me.lblREsultados = New System.Windows.Forms.Label()
+        CType(Me.pcbGrafico, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pcbGrafico
+        '
+        Me.pcbGrafico.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pcbGrafico.Location = New System.Drawing.Point(177, 12)
+        Me.pcbGrafico.Name = "pcbGrafico"
+        Me.pcbGrafico.Size = New System.Drawing.Size(345, 306)
+        Me.pcbGrafico.TabIndex = 0
+        Me.pcbGrafico.TabStop = False
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.lblPuntos)
+        Me.Panel1.Location = New System.Drawing.Point(22, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(149, 306)
+        Me.Panel1.TabIndex = 1
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.lblREsultados)
+        Me.Panel2.Location = New System.Drawing.Point(177, 324)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(345, 100)
+        Me.Panel2.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(58, 363)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'lblPuntos
+        '
+        Me.lblPuntos.AutoSize = True
+        Me.lblPuntos.Location = New System.Drawing.Point(15, 14)
+        Me.lblPuntos.Name = "lblPuntos"
+        Me.lblPuntos.Size = New System.Drawing.Size(39, 13)
+        Me.lblPuntos.TabIndex = 0
+        Me.lblPuntos.Text = "Label1"
+        '
+        'lblREsultados
+        '
+        Me.lblREsultados.AutoSize = True
+        Me.lblREsultados.Location = New System.Drawing.Point(3, 44)
+        Me.lblREsultados.Name = "lblREsultados"
+        Me.lblREsultados.Size = New System.Drawing.Size(39, 13)
+        Me.lblREsultados.TabIndex = 0
+        Me.lblREsultados.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(542, 433)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.pcbGrafico)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.pcbGrafico, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents pcbGrafico As System.Windows.Forms.PictureBox
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents lblPuntos As System.Windows.Forms.Label
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents lblREsultados As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ Analizis 1/Analizis 1/Form1.vb
@@ -0,0 +1,110 @@
+﻿Public Class Form1
+    'Variables de graficacion
+    Dim pluma As New Pen(Brushes.Gray, 0.5)
+    Dim grafico As Graphics
+    Dim px, py, vx, vy As Integer
+    Dim camino As Drawing2D.GraphicsPath
+    Sub P1()
+
+        Dim dibujo As New Bitmap(Me.pcbGrafico.Size.Width, Me.pcbGrafico.Size.Height)
+        pcbGrafico.Image = dibujo
+        grafico = Graphics.FromImage(Me.pcbGrafico.Image)
+        grafico.FillRectangle(Brushes.White, 0, 0, 345, 306)
+        px = pcbGrafico.Width / 2
+        py = pcbGrafico.Height / 2
+        'Linea x del plano cartesiano
+        grafico.DrawLine(Pens.Black, 0, py, pcbGrafico.Width, py)
+        'linea y del plano cartesiano
+        grafico.DrawLine(Pens.Black, px, 0, px, pcbGrafico.Height)
+        'Cuadricula
+        For i = 0 To Int(pcbGrafico.Width) Step 7
+            grafico.DrawLine(pluma, i, 0, i, pcbGrafico.Height)
+        Next
+        For i = 0 To Int(pcbGrafico.Height) Step 6
+            grafico.DrawLine(pluma, 0, i, pcbGrafico.Width, i)
+        Next
+
+
+
+        Dim n, pol(,) As Integer
+        MsgBox("1.- Ingrese los lados del poligono", 0, "Aviso de llenado")
+        n = Val(InputBox("Ingrese la cantidad de lados del poligono: ", "Lados del poligono"))
+        'n = n * 2
+        n = n
+        ReDim pol(n, 1)
+        lblPuntos.Text = ""
+        For i = 0 To n Step 1
+            For j = 0 To 1 Step 1
+                If (i = 0) Then
+                    pol(i, j) = Val(InputBox("Ingrese valor X Para el punto: " & i + 1, "X " & i + 1))
+                    pol(n, j) = pol(i, j)
+                ElseIf(i<n)
+                    pol(i, j) = Val(InputBox("Ingrese valor X Para el punto: " & i + 1, "X " & i + 1))
+                End If
+                If j = 0 Then
+                    lblPuntos.Text = lblPuntos.Text & "P" & i + 1 & ": " & pol(i, j) & ", "
+                Else
+                    lblPuntos.Text = lblPuntos.Text & pol(i, j) & vbNewLine
+                End If
+               
+            Next
+            If i > 0 Then
+                vx = py - pol(i - 1, 1) * 10
+                vy = px + pol(i - 1, 0) * 10
+                grafico.DrawLine(Pens.Red, vy, vx, px + pol(i, 0) * 10, py - pol(i, 1) * 10)
+                pcbGrafico.Image = dibujo
+            End If
+        Next
+        'Productos Cruzados
+        Dim p1, p2, con As Integer
+        p1 = pol(0, 0) * pol(1, 1)
+        con = 1
+        For i = 1 To n - 1 Step 1
+
+            If con = 0 Then
+                p1 = p1 * pol(i + 1, 1)
+                con = 1
+            ElseIf con = 1 Then
+                p1 = p1 * pol(i + 1, 0)
+                con = 0
+            End If
+        Next
+        p2 = pol(n, 1) * pol(n - 1, 0)
+        con = 0
+        For i = n - 1 To 1 Step -1
+            If con = 0 Then
+                p2 = p2 * pol(i - 1, 1)
+                con = 1
+            ElseIf con = 1 Then
+                p2 = p2 * pol(i - 1, 0)
+                con = 0
+            End If
+        Next
+
+        lblREsultados.Text = "Productos cruzados 1= " & p1 & vbNewLine & "Preductos cruzados 2 = " & p2
+    End Sub
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        P1()
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim dibujo As New Bitmap(Me.pcbGrafico.Size.Width, Me.pcbGrafico.Size.Height)
+        pcbGrafico.Image = dibujo
+        grafico = Graphics.FromImage(Me.pcbGrafico.Image)
+        grafico.FillRectangle(Brushes.White, 0, 0, 345, 306)
+        px = pcbGrafico.Width / 2
+        py = pcbGrafico.Height / 2
+        'Linea x del plano cartesiano
+        grafico.DrawLine(Pens.Black, 0, py, pcbGrafico.Width, py)
+        'linea y del plano cartesiano
+        grafico.DrawLine(Pens.Black, px, 0, px, pcbGrafico.Height)
+        'Cuadricula
+        For i = 0 To Int(pcbGrafico.Width) Step 7
+            grafico.DrawLine(pluma, i, 0, i, pcbGrafico.Height)
+        Next
+        For i = 0 To Int(pcbGrafico.Height) Step 6
+            grafico.DrawLine(pluma, 0, i, pcbGrafico.Width, i)
+        Next
+    End Sub
+End Class
